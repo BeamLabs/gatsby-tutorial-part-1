35ecaf6112d6e5dda3c33b59375237e30ed572da
Andre Giron <agiron123@gmail.com>
2019-10-03 13:51:56 -0400

    Updates

---
+++ component---src-pages-about-js-101888af5d7b778ab8f0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{190:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(193);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}},193:function(e,t,n){"use strict";var a=n(0),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-101888af5d7b778ab8f0.js.map
