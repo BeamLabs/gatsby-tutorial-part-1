6a7bd53271babde28662aa82e5156a49330b3430
Andre Giron <agiron123@gmail.com>
2019-10-03 13:15:26 -0400

    Finished code from part 1 of the gatsby tutorial.

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: 'teal' }}>
+    <Header headerText="404 Page Not Found!" />
+    <p>We're sorry, but we cannot find that page.</p>
+    <p>Here, let's get you back safely.</p>
+  </div>
+)
